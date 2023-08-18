47d3b032bdb2fce73ef3f8f8f7a929b29f4503d7
wenge8n <wenge8n@outlook.com>
2023-08-18 17:38:52 +0800

    Update pdf viewer

--- src/screens/PDFViewer/index.js
+++ src/screens/PDFViewer/index.js
@@ -1,28 +1,46 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Platform, Text, View} from 'react-native';
 import Pdf from 'react-native-pdf';
+import PSPDFKitView from 'react-native-pspdfkit';
 import styles from './styles';
+
+const DOCUMENT =
+  Platform.OS === 'ios' ? 'Sample.pdf' : 'file:///android_asset/Sample.pdf';
 
 const PDFViewer = ({route}) => {
   return (
     <View style={styles.container}>
-      <Pdf
-        source={{
-          uri: route.params.uri,
-          cache: true,
-        }}
-        onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`Number of pages: ${numberOfPages}`);
-        }}
-        onError={error => {
-          console.log(error);
-        }}
-        style={styles.pdf}
-        renderActivityIndicator={progress => (
-          <Text>{(progress * 100).toFixed()}%</Text>
-        )}
-        trustAllCerts={false}
-      />
+      {false ? (
+        <Pdf
+          source={{
+            uri: route.params.uri,
+            cache: true,
+          }}
+          onLoadComplete={numberOfPages => {
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onError={error => {
+            console.log(error);
+          }}
+          style={styles.pdf}
+          renderActivityIndicator={progress => (
+            <Text>{(progress * 100).toFixed()}%</Text>
+          )}
+          trustAllCerts={false}
+        />
+      ) : (
+        <PSPDFKitView
+          document={DOCUMENT}
+          showNavigationButtonInToolbar={false}
+          showCloseButton={false}
+          configuration={{
+            showThumbnailBar: 'scrollable',
+            pageTransition: 'scrollContinuous',
+            scrollDirection: 'vertical',
+          }}
+          style={styles.pdf}
+        />
+      )}
     </View>
   );
 };
